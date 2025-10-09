9f56e46c3741583c5098e4fa3a2ce55e242ad82d
MULLAIVENDAN22 <kmullai2223@gmail.com>
2025-10-09 21:27:47 +0530

    jayam web solution stock and wishlist logic  addedin home page

--- Jayam web solution/moto admin/home/script.js
+++ Jayam web solution/moto admin/home/script.js
@@ -3,55 +3,53 @@ const mainImgArray = [
   "https://motaadmin.dexignlab.com/codeigniter/demo/public/assets/frontend/images/home-banner/media-men2.png",
 ];
 
-const mininavArray = [
-  {
-    img: "https://motaadmin.dexignlab.com/codeigniter/demo/public/assets/frontend/images/product/pic1.png",
-    h6: "Collar Regular Fit T-Shirt",
-    h5: "$4.00",
-    span: "$70.00",
-    type: "shirt",
-  },
-  {
-    img: "https://motaadmin.dexignlab.com/codeigniter/demo/public/assets/frontend/images/product/pic2.png",
-    h6: "Men's Wonder Shoes",
-    h5: "$8.00 ",
-    span: "$12.00",
-    type: "shoe",
-  },
-  {
-    img: "https://motaadmin.dexignlab.com/codeigniter/demo/public/assets/frontend/images/product/pic3.png",
-    h6: "Collar Regular Fit T-Shirt",
-    h5: "$4.00",
-    span: "",
-    type: "shirt",
-  },
-  {
-    img: "https://motaadmin.dexignlab.com/codeigniter/demo/public/assets/frontend/images/product/pic4.png",
-    h6: "Men's Wonder Watch",
-    h5: "$8.00 ",
-    span: "$12.00",
-    type: "watch",
-  },
-  {
-    img: "https://motaadmin.dexignlab.com/codeigniter/demo/public/assets/frontend/images/product/pic5.png",
-    h6: "Men's Wonder Blazer",
-    h5: "$6.00",
-    span: "$12.00",
-    type: "blazer",
-  },
-  {
-    img: "https://motaadmin.dexignlab.com/codeigniter/demo/public/assets/frontend/images/product/pic6.png",
-    h6: "Collar Regular Fit T-Shirt",
-    h5: "$6.00",
-    span: "",
-    type: "bag",
-  },
-];
+let mininavArray;
 
-const getUserAccount = JSON.parse(window.localStorage.getItem("userAccount" || "[]"));
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch("https://dummyjson.com/c/811b-f8f0-4b2e-ada1");
+    const data = await response.json();
+    console.log(data.products);
+    mininavArray = data.products;
+  } catch (error) {
+    console.log(error);
+  }
+
+  mininavArray.forEach((value) => {
+    handle(value);
+  });
+});
+
+let currentUserIndex = -1;
+const getUserAccount = JSON.parse(
+  window.localStorage.getItem("userAccount" || "[]")
+);
 if (!getUserAccount.some((element) => element.status == true)) {
   window.location.replace("../index.html");
 }
+
+const currentUser = document.querySelectorAll(".currentUserAccount");
+getUserAccount.forEach((element, index) => {
+  if (element.status == true) {
+    currentUserIndex = index;
+    currentUser[0].innerHTML = `HI, ${element.username.toUpperCase()}`;
+    currentUser[1].innerHTML = `HI, ${element.username.toUpperCase()}`;
+    return;
+  }
+});
+
+const profile = document.querySelector(".profile");
+profile.addEventListener("click", () => {
+  document.querySelector(".profile-action").classList.toggle("d-none");
+});
+
+const signout = document.querySelector(".signout");
+signout.addEventListener("click", () => {
+  document.querySelector(".profile-action").classList.toggle("d-none");
+  getUserAccount[currentUserIndex].status = false;
+  window.localStorage.setItem("userAccount", JSON.stringify(getUserAccount));
+  window.location.href = "../index.html";
+});
 
 let mainImgCount = [0];
 document.addEventListener("DOMContentLoaded", () => {
@@ -79,9 +77,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  mininavArray.forEach((value) => {
-    handle(value);
-  });
   const mininav = document.querySelector(".mini-nav");
   mininav.querySelectorAll("a").forEach((value) => {
     value.addEventListener("click", (e) => {
@@ -111,16 +106,25 @@ function navHandle(e) {
     });
   }
 }
+
+function disabled(val) {
+  return Number(val.stock) < 1 ? "disabled" : "";
+}
 function handle(val) {
   mininavcards.innerHTML += `
     <div
-              class="cards d-flex flex-column align-items-center my-2"
+              class="cards position-relative d-flex flex-column align-items-center my-2"
               style="width: 31%"
+              data-id = ${val.id}
             >
               <div
                 class="d-flex justify-content-center align-items-center p-5"
                 style="background-color: #EAE9E9"
               >
+              <div class="position-absolute p-4" style="top: 0px; right: 0px; z-index: 10;">
+                <i class="fa-solid fa-heart fs-4 wishlist" style="color: #6f6f6f;"></i>
+              </div>
+
                 <img
                   class="img-fluid"
                   src="${val.img}"
@@ -138,6 +142,54 @@ function handle(val) {
                 ${val.span}  </span
                 >
               </h5>
+              <div class="d-flex flex-row align-item-center" style="gap: 20%">
+              <h5 class="fw-medium text-primary pt-2" style="white-space: nowrap;">${val.stock} left</h5>
+              <div>
+                <button class="btn btn-secondary shop-icon fs-5 fw-semibold"  ${disabled(val)}>
+                  <i class="fa-solid fa-cart-shopping "></i>
+                </button>
+              </div>
+            </div>
             </div>
     `;
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("wishlist")) {
+    console.log("wish clicked");
+    wishlist(e.target);
+  }
+  if (e.target.closest(".shop-icon")) {
+    console.log("shop-icon clicked");
+    shopIcon(e.target);
+  }
+});
+
+function wishlist(e) {
+  e.classList.toggle("wish-active");
+  mininavArray[e.closest(".cards").dataset.id].wishlist =
+    !mininavArray[e.closest(".cards").dataset.id].wishlist;
+  console.log(mininavArray);
+  window.localStorage.setItem("useractivity", JSON.stringify(mininavArray));
+}
+
+function shopIcon(e) {
+  if (
+    Number(mininavArray[e.closest(".cards").dataset.id].count) >
+    Number(mininavArray[e.closest(".cards").dataset.id].stock) - 1
+  ) {
+    return;
+  }
+  if (!e.classList.contains("shop-icon")) {
+    e = e.closest(".shop-icon");
+  }
+  const img = e.innerHTML;
+  mininavArray[e.closest(".cards").dataset.id].count =
+    parseInt(mininavArray[e.closest(".cards").dataset.id].count) + 1;
+  e.innerHTML = `${mininavArray[e.closest(".cards").dataset.id].count}`;
+  setTimeout(() => {
+    e.innerHTML = img;
+  }, 500);
+  console.log(mininavArray);
+  window.localStorage.setItem("useractivity", JSON.stringify(mininavArray));
 }
